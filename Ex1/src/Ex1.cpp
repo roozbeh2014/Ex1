49d28635e21bfb4820d33cd136d4adb21741379c
roozbeh <roozbeh.manshaei@ryerson.ca>
2015-10-21 08:24:36 -0400

    error solved

---
+++ Ex1/src/Ex1.cpp
@@ -0,0 +1,57 @@
+//============================================================================
+// Name        : Ex1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+		int x = 16;
+	//	int y = x/3;
+		cout << x/3 << "\n" ;
+	//	cout << y << "\n";
+	//	cout << x/2.5 << "\n";
+
+
+	//	double z = 7;
+	//	cout << z << "\n";
+	//	cout << z/2 << "\n";
+
+//	char name;
+//	cin >> name;
+//	cout << atoi(name);
+
+//	int sumNums = 0;
+//
+//	char x[20];
+//	cout << "Enter the cancer name:";
+//	cin >> x;
+//	char cMyCharacter = x[0];
+//	int iMyAsciiValue = static_cast<int>(cMyCharacter);
+//	cout << iMyAsciiValue;
+
+//string array[10],t;
+//	cout << (int) atoi(argv[1]) << endl;
+
+//	for (int i=0; i<argc-2;i++){
+
+//		if (static_cast<int>(argv[i][0]) > static_cast<int>(argv[i+1][0])){
+
+//			for (int j=1; j<argc-2;j++){
+
+//				t = array [i];
+//				array[i] = array[j];
+//				array[j] = t;
+
+//			}
+//		}
+//	}
+//	cout << array[0] << endl;
+//	return 0;
+}
